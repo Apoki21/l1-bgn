d7157ea1e1fa529eedb532248f51a173b8753fde
apoki21 <a.nabilmudzakki@gmail.com>
2021-01-20 13:53:04 +0700

    g

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -24,54 +24,22 @@
                 <ul class="dropdown-menu">
                     <li><a class="sidebar-link" href="/basic-table">Upsale</a></li>
                     <li><a class="sidebar-link" href="/datatable">Handover</a></li>
-                    <li><a class="sidebar-link" href="/basic-table">Manage Support</a></li>
-                    <li><a class="sidebar-link" href="/datatable">Mass Disruption</a></li>
-                    <li><a class="sidebar-link" href="/basic-table">Customer VIP</a></li>
                     <li><a class="sidebar-link" href="/datatable">RFO & RCA</a></li>
+                    <li><a class="sidebar-link" href="/basic-table">Customer VIP</a></li>
+                    <li><a class="sidebar-link" href="/datatable">Mass Disruption</a></li>
                     <li class="nav-item dropdown"><a class="dropdown-toggle" href="javascript:void(0);"><span class="title">Monthly Reports</span><span class="arrow"><i class="ti-angle-right"></i></span></a>
                         <ul class="dropdown-menu">
                             <li><a class="sidebar-link" href="/basic-table">Customer monthly Report</a></li>
                             <li><a class="sidebar-link" href="/datatable">Send Performance Report</a></li>
                         </ul>
                     </li>
+                    <li><a class="sidebar-link" href="/basic-table">Manage Support</a></li>
+                    <li><a class="sidebar-link" href="/datatable">Knowledge Base</a></li>
                     <li><a class="sidebar-link" href="/datatable">Schedule Sharing Session </a></li>
-                    <li><a class="sidebar-link" href="/datatable">KB L1 & L2</a></li>
-                    <li><a class="sidebar-link" href="/datatable">RFO & RCA</a></li>
+                    
                 </ul>
             </li>
             <li class="nav-item dropdown"><a class="sidebar-link" href="/ui"><span class="icon-holder"><i class="c-pink-500 ti-palette"></i> </span><span class="title">Users</span></a></li>
-            <li class="nav-item dropdown"><a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-holder"><i class="c-orange-500 ti-layout-list-thumb"></i> </span><span class="title">Tables</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="sidebar-link" href="/basic-table">Basic Table</a></li>
-                    <li><a class="sidebar-link" href="/datatable">Data Table</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown"><a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-holder"><i class="c-purple-500 ti-map"></i> </span><span class="title">Maps</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="/google-maps">Google Map</a></li>
-                    <li><a href="/vector-maps">Vector Map</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown"><a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-holder"><i class="c-red-500 ti-files"></i> </span><span class="title">Pages</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="sidebar-link" href="/blank">Blank</a></li>
-                    <li><a class="sidebar-link" href="/404">404</a></li>
-                    <li><a class="sidebar-link" href="/500">500</a></li>
-                    <li><a class="sidebar-link" href="/signin">Sign In</a></li>
-                    <li><a class="sidebar-link" href="/signup">Sign Up</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown"><a class="dropdown-toggle" href="javascript:void(0);"><span class="icon-holder"><i class="c-teal-500 ti-view-list-alt"></i> </span><span class="title">Multiple Levels</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                <ul class="dropdown-menu">
-                    <li class="nav-item dropdown"><a href="javascript:void(0);"><span>Menu Item</span></a></li>
-                    <li class="nav-item dropdown"><a href="javascript:void(0);"><span>Menu Item</span> <span class="arrow"><i class="ti-angle-right"></i></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="javascript:void(0);">Menu Item</a></li>
-                            <li><a href="javascript:void(0);">Menu Item</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
         </ul>
     </div>
 </div>
